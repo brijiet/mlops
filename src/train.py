fb6ab34219d21e27d1fe22e4c670032a02232b5c
ESSLU <esssltn@gmail.com>
2026-08-19 13:14:32 +0530

    initial commit

---
+++ src/train.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
+def main():
+
+    data = load_breast_cancer()
+
+    X = pd.DataFrame(
+        data.data,
+        columns=data.feature_names
+    )
+
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
+
+    model = RandomForestClassifier(
+        n_estimators=100,
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+
+    predictions = model.predict(X_test)
+
+    accuracy = accuracy_score(
+        y_test,
+        predictions
+    )
+
+    print("Model Accuracy:", accuracy)
+
+
+if __name__ == "__main__":
+    main()
